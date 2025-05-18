bbdaa94a0908000f72ea4b13ddd77c906582f301
Dibyanshu Mohanty <123383178+Dibyanshu44@users.noreply.github.com>
2025-05-18 16:31:36 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-var flip=Math.floor(Math.random()*2)+1;
-if(flip===1){
-    document.querySelector(".vid").setAttribute("src","head.mp4");
-}
-else{
-    document.querySelector(".vid").setAttribute("src","tail.mp4");
+var h=0;
+var t=0;
+function toss(){   
+    var flip=Math.floor(Math.random()*2)+1;
+    if(flip===1){
+        document.querySelector(".vid").setAttribute("src","head.mp4");
+        h++;
+    }
+    else{
+        document.querySelector(".vid").setAttribute("src","tail.mp4");
+        t++;
+    }
+    setTimeout(function fun() {
+        document.querySelector(".heads").innerHTML="Heads:"+h;
+        document.querySelector(".tails").innerHTML="Tails:"+t;
+    },1800);
 }
